8e819e78aedf3b85bb72e2a13cc57ceb6c98a69b
Jonathan Niklasson <jonathan.niklasson@gmail.com>
2013-09-12 10:25:24 +0200

    SQL interface implementation

--- src/CCGuestbook/CCGuestbook.php
+++ src/CCGuestbook/CCGuestbook.php
@@ -4,7 +4,7 @@
  * 
  * @package MuffinCore
  */
-class CCGuestbook extends CObject implements IController {
+class CCGuestbook extends CObject implements IController,IHasSQL {
 
   private $pageTitle = 'Muffin Guestbook Example';
   private $pageHeader = '<h1>Guestbook Example</h1><p>Showing off how to implement a guestbook. Now saving to database.</p>';
@@ -17,11 +17,69 @@ class CCGuestbook extends CObject implements IController {
   public function __construct() {
     parent::__construct();
   }
+/**
+* Implementing interface IHasSQL. Encapsulate all SQL used by this class.
+*
+* @param string $key the string that is the key of the wanted SQL-entry in the array.
+*/
+  public static function SQL($key=null) {
+     $queries = array(
+        'create table guestbook'  => "CREATE TABLE IF NOT EXISTS Guestbook (id INTEGER PRIMARY KEY, entry TEXT, created DATETIME default (datetime('now')));",
+        'insert into guestbook'   => 'INSERT INTO Guestbook (entry) VALUES (?);',
+        'select * from guestbook' => 'SELECT * FROM Guestbook ORDER BY id DESC;',
+        'delete from guestbook'   => 'DELETE FROM Guestbook;',
+     );
+     if(!isset($queries[$key])) {
+        throw new Exception("No such SQL query, key '$key' was not found.");
+      }
+      return $queries[$key];
+   }
+
+/**
+* Save a new entry to database.
+*/
+  private function CreateTableInDatabase() {
+    try {
+      $this->db->ExecuteQuery(self::SQL('create table guestbook'));
+    } catch(Exception$e) {
+      die("$e<br/>Failed to open database: " . $this->config['database'][0]['dsn']);
+    }
+  }
   
 
-  /**
-   * Implementing interface IController. All controllers must have an index action.
-   */
+/**
+* Save a new entry to database.
+*/
+  private function SaveNewToDatabase($entry) {
+    $this->db->ExecuteQuery(self::SQL('insert into guestbook'), array($entry));
+    if($this->db->rowCount() != 1) {
+      echo 'Failed to insert new guestbook item into database.';
+    }
+  }
+
+/**
+* Delete all entries from the database.
+*/
+  private function DeleteAllFromDatabase() {
+    $this->db->ExecuteQuery(self::SQL('delete from guestbook'));
+  }
+  
+  
+/**
+* Read all entries from the database.
+*/
+  private function ReadAllFromDatabase() {
+    try {
+      $this->db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+      return $this->db->ExecuteSelectQueryAndFetchAll(self::SQL('select * from guestbook'));
+    } catch(Exception $e) {
+      return array();    
+    }
+  }
+ 
+/**
+* Implementing interface IController. All controllers must have an index action.
+*/
   public function Index() {	
     $formAction = $this->request->CreateUrl('guestbook/handler');
     $this->pageForm = "
@@ -47,9 +105,9 @@ class CCGuestbook extends CObject implements IController {
   }
   
 
-  /**
-   * Handle posts from the form and take appropriate action.
-   */
+/**
+* Handle posts from the form and take appropriate action.
+*/
   public function Handler() {
     if(isset($_POST['doAdd'])) {
       $this->SaveNewToDatabase(strip_tags($_POST['newEntry']));
@@ -63,47 +121,4 @@ class CCGuestbook extends CObject implements IController {
     header('Location: ' . $this->request->CreateUrl('guestbook'));
   }
   
-
-  /**
-   * Save a new entry to database.
-   */
-  private function CreateTableInDatabase() {
-    try {
-      $this->db->ExecuteQuery("CREATE TABLE IF NOT EXISTS Guestbook (id INTEGER PRIMARY KEY, entry TEXT, created DATETIME default (datetime('now')));");
-    } catch(Exception$e) {
-      die("$e<br/>Failed to open database: " . $this->config['database'][0]['dsn']);
-    }
-  }
-  
-
- /**
-   * Save a new entry to database.
-   */
-  private function SaveNewToDatabase($entry) {
-    $this->db->ExecuteQuery('INSERT INTO Guestbook (entry) VALUES (?);', array($entry));
-    if($this->db->rowCount() != 1) {
-      echo 'Failed to insert new guestbook item into database.';
-    }
-  }
-
-  /**
-   * Delete all entries from the database.
-   */
-  private function DeleteAllFromDatabase() {
-    $this->db->ExecuteQuery('DELETE FROM Guestbook;');
-  }
-  
-  
-/**
-   * Read all entries from the database.
-   */
-  private function ReadAllFromDatabase() {
-    try {
-      $this->db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-      return $this->db->ExecuteSelectQueryAndFetchAll('SELECT * FROM Guestbook ORDER BY id DESC;');
-    } catch(Exception $e) {
-      return array();    
-    }
-  }
-  
-} 
+}
