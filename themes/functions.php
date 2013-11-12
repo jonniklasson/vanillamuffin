2fe7dedafff59dc5ecaac605a359c6fe69999412
Jonathan Niklasson <jonathan.niklasson@gmail.com>
2013-11-12 08:23:14 +0100

    gravatar

--- themes/functions.php
+++ themes/functions.php
@@ -75,7 +75,7 @@ function theme_url($url) {
 function login_menu() {
   $mu = CMuffin::Instance();
   if($mu->user['isAuthenticated']) {
-    $items = "<a href='" . create_url('user/profile') . "'>" . $mu->user['acronym'] . "</a> ";
+    $items = "<a href='" . create_url('user/profile') . "'><img class='gravatar' src='" . get_gravatar(20) . "' alt=''> " . $mu->user['acronym'] . "</a> ";
     if($mu->user['hasRoleAdministrator']) {
       $items .= "<a href='" . create_url('acp') . "'>acp</a> ";
     }
@@ -83,8 +83,16 @@ function login_menu() {
   } else {
     $items = "<a href='" . create_url('user/login') . "'>login</a> ";
   }
-  return "<nav>$items</nav>";
-}
+   return "<nav id='login-menu'>$items</nav>";
+ }
+ 
+ 
+ /**
+  * Get a gravatar based on the user's email.
+  */
+ function get_gravatar($size=null) {
+   return 'http://www.gravatar.com/avatar/' . md5(strtolower(trim(CMuffin::Instance()->user['email']))) . '.jpg?r=pg&amp;d=wavatar&amp;' . ($size ? "s=$size" : null);
+ }
 
 /**
 * Get messages stored in flash-session.
